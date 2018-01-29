5193f8635d1cceaab3c6fb944f558c8aaf109528
jassonjunior <jassonjunior@gmail.com>
2018-01-29 19:27:47 -0200

    Inicio

---
+++ listavip/src/main/java/com/br/listavip/Configuracao.java
@@ -0,0 +1,23 @@
+package com.br.listavip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class Configuracao {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Configuracao.class, args);
+	}
+	
+	@RequestMapping("/")
+	@ResponseBody
+	String ola(){
+		return "Ola, teste";
+	}
+	
+}
